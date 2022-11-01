d75d4c4e7cdda42447c2089b53ea180b6e2bc063
leozbroca <leo.brokka@gmail.com>
2022-11-01 00:00:09 -0300

    deploy

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,6 @@ app.post("/login", userController.login)
 
 app.post("/signupCar", carController.signupCar)
 app.put("/editCar/:id", carController.editCar)
-app.delete("/deleteCar/:id", carController.deleteCar)
 app.get("/allcars", carController.getAllCars)
 app.get("/getMyCars", carController.getCarsByUser)
+app.delete("/deleteCar/:id", carController.deleteCar)
