38d22e2b0ddd217e440b66d1cf0a86b515e4b5d5
leozbroca <leo.brokka@gmail.com>
2022-11-01 07:52:58 -0300

    deploy

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import app from "./app";
 import userController from "./controller/userController"
 import carController from "./controller/carController"
 
+// Requisições usuário
 app.post("/signup", userController.signup)
 app.post("/login", userController.login)
 
+//Requisições carros
 app.post("/signupCar", carController.signupCar)
 app.put("/editCar/:id", carController.editCar)
 app.get("/allcars", carController.getAllCars)
